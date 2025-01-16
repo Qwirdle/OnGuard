c20a9ce2f3b8a10ae5057aa438c038c061736025
Qwirdle <90058993+Qwirdle@users.noreply.github.com>
2025-01-16 14:26:15 -0500

    omg i have no idea what i did but it was something

---
+++ OnGuard-main/src/config.py
@@ -0,0 +1,6 @@
+# If you're looking at this and thinking, huh, we should put this into a .env file, DON'T
+# This is a small project, and it's going to be hard to manouver around dealing with all the github commits
+
+FLASK_DEBUG = True
+SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite"
+SECRET_KEY = "XFYXGK3tQD52O5pgzeCfj4c7gWiFFAmZ"
